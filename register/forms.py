deb44ebef1adaab710b6ac6c81290fd219469f7f
psachin <iclcoolster@gmail.com>
2013-04-17 15:26:22 +0530

    Subject: added phone number field(max_value=9999999999)
    Description:

--- register/forms.py
+++ register/forms.py
@@ -8,11 +8,12 @@ from string import letters
 
 class Register(UserCreationForm):
     """my form"""
-    serial_num = forms.CharField(max_length=10)
+    serial_num = forms.CharField(max_length=10, required=True)
+    phone_num = forms.IntegerField(max_value=9999999999, required=True)
     
     class Meta:
         model = User
-        fields = ('username','first_name','last_name','email','serial_num','password1','password2',)
+        fields = ('username','first_name','last_name','email','serial_num','phone_num','password1','password2')
 
         
     def save(self, commit=True):
@@ -29,6 +30,7 @@ class Register(UserCreationForm):
         if commit:
             profile = user.get_profile()
             profile.serial_num = self.cleaned_data['serial_num']
+            profile.phone_num = self.cleaned_data['phone_num']
             # profile.serial_num = ''.join([choice(letters) for i in xrange(12)])
             #user_profile = UserProfile(user=user, serial_num=self.cleaned_data['serial_num'])
             profile.save()
--- register/models.py
+++ register/models.py
@@ -5,6 +5,7 @@ from django.db.models.signals import post_save
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
     serial_num = models.CharField(max_length=10)
+    phone_num = models.BigIntegerField(null=True)
 
     def __unicode__(self):
         return "%s" % self.user
